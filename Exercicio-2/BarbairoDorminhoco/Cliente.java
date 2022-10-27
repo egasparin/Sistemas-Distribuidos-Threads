7d9228bd5597adc456e099c872bbc288158de429
Guilherme Huff <ghuff@alunos.utfpr.edu.br>
2022-10-27 09:25:43 -0300

    inicio ex 2

---
+++ Exercicio-2/BarbairoDorminhoco/Cliente.java
@@ -0,0 +1,5 @@
+package BarbairoDorminhoco;
+
+public class Cliente {
+
+}
---
+++ Exercicio-2/BarbairoDorminhoco/Main.java
@@ -0,0 +1,7 @@
+package BarbairoDorminhoco;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("TEste");
+	}
+}
